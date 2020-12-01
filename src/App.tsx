ca1a04e74fa51dfa7de80bc248c257cbb3f3b0ec
Shreesh Kulkarni <sgkul2000@gmail.com>
2020-12-01 13:06:02 +0530

    Fix minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './assets/css/App.css'
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import Login from './components/Login'
 import User from './entity/User'
